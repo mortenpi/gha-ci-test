f9b7989ad08eb6222de2b59c1fafbde1ec7084de
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-06-01 18:04:18 +1200

    add dydlenv

--- test.jl
+++ test.jl
@@ -1,3 +1,10 @@
 using Git
 
+# https://stackoverflow.com/a/56248596/1601695
+dyldenv = [
+    "DYLD_PRINT_LIBRARIES=1",
+    "DYLD_PRINT_LIBRARIES_POST_LAUNCH=1",
+   "DYLD_PRINT_RPATHS=1"
+]
+
 run(`$(git()) submodule add foo bar`)
